10175daf4c926889b4b839e8c02520499c277018
lprtan <lprntan@gmail.com>
2024-03-11 17:53:59 +0300

    add film list

--- src/app/movies/movies.companent.ts
+++ src/app/movies/movies.companent.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
-
+import { Movies } from "../movie.datasource";
 @Component({
     selector: 'movies', // <movies></movies>
     templateUrl: 'movies.component.html'
 })
 export class MoviesCompanent {
-
+    title='Movie List';
+    movies= Movies;
 }
